dc77f3de7bfc1e7fbcc4b05d73777c8ad016e067
Artemiy Smirnov <artemiy.smirnov@htlstp.at>
2023-10-30 17:51:30 +0100

    Solved 2022 Day 14

---
+++ src/main/kotlin/solutions/y2022/Day14.kt
@@ -0,0 +1,102 @@
+package solutions.y2022
+
+import Solution
+import utils.grid.Grid
+import utils.point.Point
+import java.io.File
+
+class Day14 : Solution(2022) {
+
+    private val origin = Point(500, 0)
+
+    override fun solvePart1(input: File): Int {
+        val cave = parse(input.readLines())
+
+        var sandCount = 0
+        var currentPos: Point
+        var nextPos: Point
+
+        outer@ while (true) {
+            currentPos = origin.copy()
+            nextPos = currentPos.up
+
+            inner@ while (true) {
+                if (nextPos.y > cave.maxY) break@outer
+
+                if (cave.getOrDefault(nextPos, '.') == '.') {
+                    currentPos = nextPos
+                    nextPos = currentPos.up
+                } else if (cave.getOrDefault(currentPos.upLeft, '.') == '.') {
+                    currentPos = currentPos.upLeft
+                    nextPos = currentPos.up
+                } else if (cave.getOrDefault(currentPos.upRight, '.') == '.') {
+                    currentPos = currentPos.upRight
+                    nextPos = currentPos.up
+                } else break@inner
+            }
+
+            cave[currentPos] = 'o'
+            sandCount++
+        }
+
+        return sandCount
+    }
+
+    override fun solvePart2(input: File): Int {
+        val cave = parse(input.readLines())
+
+        var sandCount = 0
+        var currentPos: Point
+        var nextPos: Point
+        val yLimit = cave.maxY + 2
+
+        outer@ while (true) {
+            currentPos = origin.copy()
+            nextPos = currentPos.up
+
+            inner@ while (true) {
+                if (cave[origin] == 'o') break@outer
+
+                if (nextPos.y == yLimit) {
+                    break@inner
+                }
+                if (cave.getOrDefault(nextPos, '.') == '.') {
+                    currentPos = nextPos
+                    nextPos = currentPos.up
+                } else if (cave.getOrDefault(currentPos.upLeft, '.') == '.') {
+                    currentPos = currentPos.upLeft
+                    nextPos = currentPos.up
+                } else if (cave.getOrDefault(currentPos.upRight, '.') == '.') {
+                    currentPos = currentPos.upRight
+                    nextPos = currentPos.up
+                } else break@inner
+            }
+
+            cave[currentPos] = 'o'
+            sandCount++
+        }
+        return sandCount
+    }
+
+    private fun parse(lines: List<String>): Grid<Char> {
+        val rocks = lines.map { line ->
+            line.split(" -> ")
+                .map { it.split(",").let { pos -> Point(pos[0].toInt(), pos[1].toInt()) } }
+        }
+
+        val grid = Grid<Char>().apply {
+            rocks.forEach { points ->
+                points.windowed(2).forEach { (start, end) ->
+                    if (start.x == end.x) {
+                        val yRange = if (start.y <= end.y) start.y..end.y else end.y..start.y
+                        yRange.forEach { y -> this[Point(start.x, y)] = '#' }
+                    } else if (start.y == end.y) {
+                        val xRange = if (start.x <= end.x) start.x..end.x else end.x..start.x
+                        xRange.forEach { x -> this[Point(x, start.y)] = '#' }
+                    }
+                }
+            }
+        }
+        return grid
+    }
+}
--- src/main/kotlin/utils/point/Point.kt
+++ src/main/kotlin/utils/point/Point.kt
@@ -5,40 +5,45 @@ import kotlin.math.abs
 data class Point(var x: Int, var y: Int) {
     constructor(x: Number, y: Number) : this(x.toInt(), y.toInt())
 
+    val up: Point get() = Point(x, y + 1)
+    val upRight: Point get() = Point(x + 1, y + 1)
+    val right: Point get() = Point(x + 1, y)
+    val downRight: Point get() = Point(x + 1, y - 1)
+    val down: Point get() = Point(x, y - 1)
+    val downLeft: Point get() = Point(x - 1, y - 1)
+    val left: Point get() = Point(x - 1, y)
+    val upLeft: Point get() = Point(x - 1, y + 1)
+
     fun isInside(maxX: Int, maxY: Int): Boolean {
         return x in 0..<maxX && y in 0..<maxY
     }
 
     fun getCardinalNeighbors(): List<Point> {
-        return listOf(
-            Point(x, y - 1),  // North
-            Point(x + 1, y),  // East
-            Point(x, y + 1),  // South
-            Point(x - 1, y)   // West
-        )
+        return listOf(up, right, down, left)
+    }
+
+    fun getNeighbors(): List<Point> {
+        return listOf(up, upRight, right, downRight, down, downLeft, left, upLeft)
     }
 
     fun manhattanDistance(other: Point) = abs(x - other.x) + abs(y - other.y)
 
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
-
     operator fun unaryPlus() = Point(+x, +y)
-
     operator fun minus(other: Point) = Point(x - other.x, y - other.y)
-
     operator fun unaryMinus() = Point(-x, -y)
-
     operator fun times(other: Point) = Point(x * other.x, y * other.y)
-
     operator fun div(other: Point) = Point(x / other.x, y / other.y)
-
     operator fun rem(other: Point) = Point(x % other.x, y % other.y)
-
     operator fun inc() = Point(x + 1, y + 1)
-
     operator fun dec() = Point(x - 1, y - 1)
-
     operator fun compareTo(other: Point) = (x + y).compareTo(other.x + other.y)
-
     override fun toString() = "$x-$y"
+
+    companion object {
+        fun of(input: String): Point {
+            val (x, y) = input.split(',').map { it.trim().toInt() }
+            return Point(x, y)
+        }
+    }
 }
