880af923c987244f042288dc02045d3e92b6b61a
Artemiy Smirnov <artemiy.smirnov@proton.me>
2024-01-01 14:23:22 +0100

    fix formatting

--- src/main/kotlin/solutions/y2020/Day1.kt
+++ src/main/kotlin/solutions/y2020/Day1.kt
@@ -7,12 +7,12 @@ import utils.Utils.zipWithAllUnique
 import java.io.File
 
 class Day1 : Solution(2020) {
+
     override fun solvePart1(input: File): Any {
         return input.ril().zipWithAllUnique().first { it.first + it.second == 2020 }.let { it.first * it.second }
     }
 
     override fun solvePart2(input: File): Any {
         return input.ril().zipWithAllUnique(3).first { it.sum() == 2020 }.product()
-
     }
 }
